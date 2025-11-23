995f6cf9abdc9c3124e8c4643e2abcf19aa224e7
Christian Burmeister <mail@cburmeister.me>
2025-11-23 08:30:25 +0100

    Started "Decoding Multiple Instructions and Suffixes".

--- doc/notes.md
+++ doc/notes.md
@@ -1,6 +1,6 @@
 # Curr
 https://www.computerenhance.com/p/table-of-contents
-- Introduction: Waste
+- Decoding Multiple INstructions and Suffixes
 
 # Introduction: Five Multipliers
 1st: Waste: CPU doing a lot more instructions that are not part of the core logic. Examples include cleanup code and interpreter overhead.
@@ -15,17 +15,29 @@ https://www.computerenhance.com/p/table-of-contents
 nasm result.asm
 diff upstream/perfaware/part1/listing_0037_single_register_mov result
 
-zig build run -- upstream/perfaware/part1/listing_0038_many_register_mov > result.asm && diff upstream/perfaware/part1/listing_0038_many_register_mov.asm result.asm
+testing:
+zig build run -- upstream/perfaware/part1/listing_0039_more_movs > result.asm && nasm result.asm && diff result upstream/perfaware/part1/listing_0039_more_movs
 
 ## Doc
 https://edge.edx.org/c4x/BITSPilani/EEE231/asset/8086_family_Users_Manual_1_.pdf
-- P.160 Decoding
+- P.160 Decoding.
     
 - 16-bit Registers. Memory-to-Register Movement.
     => Register names have 16-bit (AX), high-8-bit (AH) and low-8-bit variants (AL).
     MOV AX, BX => 16-bit, MOV AL, BL
 - Instruction Decode: From byte encoding to actual work.
 - Mnemonic = Human friendly name: MOV AX,BX = Move (Operand) from Bx (Source) to Ax (Destination).
-- 8-bit Encoding: Opcode (6) + D-Flag () + W-Flag ()
-- 8-bit Opcode Parameter: MOD-Field (2), REG-Field (3), RM (3)
+- 8-bit Encoding: Opcode (6) + D-Flag (destination) + W-Flag (wide (16-bit))
+- 8-bit Opcode Parameter: MOD-Field (2), REG-Field (3), RM-Field (3)
+- 16-bit Displacement: Optional for memory data.
+- 86 architecture: variable length: Each byte can tell us if there is one more byte after that for the same instruction.
+- memory operation: mov bx, [address]
+- x86 is little endian.
+- effective address calculation: mov bx, [bp + 75] => displacement => defined by MOD-field
+- MOD-field: do we have 1-byte or 2-byte displacement? means we have more bytes coming in.
+- destination flag and memory: 1 == load (register is destination), 0 == store (register is source). 
+- RM-Field: Encodes type of equation of effective address calculation.
+    - (bx)+(si), (bx)+(di), (bp)+(si), (bp)+(di), (si), (di), (bp), (bx)  
+    - with the MOD field you add d8 or d16 to it.
+    - MOD == 0 and RM = 110 => 2 byte immediate direct address.
 
--- src/part1/8086.zig
+++ src/part1/8086.zig
@@ -1,59 +1,7 @@
 const std = @import("std");
+const assert = std.debug.assert;
 
-// TODO: Conversion from reg u3 to RFID?
-const RegField = enum(u3) {
-    al, cl, dl, bl,
-    ah, ch, dh, bh,
-};
-const RegFieldWide = enum(u3) {
-    ax, cx, dx, bx,
-    sp, bp, si, di,
-};
-
-const Opcode = enum(u6) {
-    mov_register = 0b100010,
-    _,
-
-    pub fn format(self: @This(), writer: *std.io.Writer) std.io.Writer.Error!void {
-        try writer.print("{s}", .{ 
-            switch(self) {
-                .mov_register => "mov",
-                _ => "",
-            } 
-        });
-    }
-};
-
-const Instruction = packed struct(u16) {
-    wide: bool,         // 1 == 16-bit, 0 == 8-bit 
-    dest: enum(u1) {    // destination
-        rm_is_dest,
-        reg_is_dest,
-    },
-    opcode: Opcode,     // opcode
-    rm: u3,             // register-name OR memory
-    reg: u3,            // register-name
-    mod: enum(u2) {     // register or memory operation
-        memory,
-        memory_8bit_displacement,
-        memory_16bit_displacement,
-        register,
-    },
-
-    pub fn format(self: @This(), writer: *std.io.Writer) std.io.Writer.Error!void {
-        const dest: u3, const src: u3 = switch (self.dest) {
-            .rm_is_dest => .{ self.rm, self.reg },
-            .reg_is_dest => .{ self.reg, self.rm },
-        };
-        const dest_tag = if(self.wide) @tagName(@as(RegFieldWide, @enumFromInt(dest))) 
-            else @tagName(@as(RegField, @enumFromInt(dest)));
-        const src_tag = if(self.wide) @tagName(@as(RegFieldWide, @enumFromInt(src))) 
-            else @tagName(@as(RegField, @enumFromInt(src)));
-
-        try writer.print("{f} {s}, {s}", .{ self.opcode, dest_tag, src_tag });
-    }
-};
-
+// Registers
 const RegisterFile = packed union {
     r16: packed struct {
         ax: u16,
@@ -70,22 +18,183 @@ const RegisterFile = packed union {
         cl: u8, ch: u8,
         dl: u8, dh: u8,
         bl: u8, bh: u8,
-        spl: u8, sph: u8,
-        bpl: u8, bph: u8,
-        sil: u8, sih: u8,
-        dil: u8, dih: u8,
+        spl: u8, sph: u8, // invalid
+        bpl: u8, bph: u8, // invalid
+        sil: u8, sih: u8, // invalid
+        dil: u8, dih: u8, // invalid
     },
 };
+
+// Prefix
+const Opcode = enum(u6) {
+    mov_rm,
+    mov_rm_imm,
+    mov_r_imm,
+    mov_mem_acc,
+    mov_acc_mem,
+    _,
+
+    pub fn format(self: @This(), writer: *std.io.Writer) std.io.Writer.Error!void {
+        try writer.print("{s}", .{ 
+            switch(self) {
+                .mov_rm, .mov_rm_imm, .mov_r_imm, .mov_mem_acc, .mov_acc_mem => "mov",
+                _ => "",
+            } 
+        });
+    }
+};
+// TODO: Move the infix size from the opcode. The opcode defines everything else (infix, displacement, data).
+pub fn parseOpcode(byte: u8) struct { Opcode, usize } {
+    return switch(byte) {
+        0b1000_1000...0b1000_1011 => .{ .mov_rm, 1 },
+        0b1100_0110...0b1100_0111 => .{ .mov_rm_imm, 1 },
+        0b1011_0000...0b1011_1111 => .{ .mov_r_imm, 0 },
+        0b1010_0000...0b1010_0001 => .{ .mov_mem_acc, 0 },
+        0b1010_0010...0b1010_0011 => .{ .mov_acc_mem, 0 },
+        else => undefined,
+    };
+}
+
+// Infix
+// TODO: Conversion from reg u3 to RFID?
+const RegField = enum(u3) {
+    al, cl, dl, bl,
+    ah, ch, dh, bh,
+};
+const RegFieldWide = enum(u3) {
+    ax, cx, dx, bx,
+    sp, bp, si, di,
+};
+const RegMemField = enum(u3) {
+    bx_si, bx_di, bp_si, bp_di, 
+    si, di, bp, bx,
+};
+const ModField = enum(u2) {
+    memory,                     // [...] => No displacement or 2-byte displacement iff rm == 110.
+    memory_8bit_displacement,   // [...+8] => 1 extra byte displacement.
+    memory_16bit_displacement,  // [...+16] => 2 extra byte displacement.
+    register,                   // bx
+};
+const Infix = packed struct(u8) {
+    rm: u3,         // register-name OR memory
+    reg: u3,        // register-name
+    mod: ModField,  // register or memory operation
+};
+
+
+// TODO: Multiple Instructions and Suffixes
+// first byte: prefix.
+// Homework: listing_0039
+    // Register and memory: 
+        // - Check Mod field (for memory transfers).
+        // - Instructions can now be longer depending on the mod field.
+        // load: mov bx, [75], store: mov [75], bx => bx loads [75] and [76] (2 bytes)
+        // effective address calculation: mov bx, [bp + 75]
+        // - Check mod field: if 11 => register else it must be memory and check more.
+    // Immediate-register-move: move ax, 12 
+        // Here we have only one "base-byte".
+    // Signed immediates: just always print the unsigned value.
+// Challenge-Homework: listing_0040
+    // mov immediate to memory: mov [BP+75], 12: Do we write 16-bits or 8-bits?
+    // => mov [BP+75],byte12 or mov [BP+75],word12 (sets w flag!). 
+    // => implement all moves in the manual but not segment register versions. 
+
+pub fn getOpcode(prefix: u8) Opcode {
+    return switch(prefix) {
+        0b1000_1000...0b1000_1011 => .mov_rm,
+        0b1100_0110...0b1100_0111 => .mov_rm_imm,
+        0b1011_0000...0b1011_1111 => .mov_r_imm,
+        0b1010_0000...0b1010_0001 => .mov_mem_acc,
+        0b1010_0010...0b1010_0011 => .mov_acc_mem,
+        else => unreachable,
+    };
+}
+
+const Fields = struct {
+    // TODO: Maybe src and dest are tagged unions? for registers, memory calculations and immediates?
+    // Then we don't need to switch later again.
+    dest: u3,
+    src: ?u3 = null,
+    wide: bool,
+    rm: ?u3 = null,     // register-name OR memory
+    reg: ?u3 = null,    // register-name
+    mod: ?ModField = null,
+    displacement: ?u16 = null,
+    data: ?u16 = null,
+    len: u3,
+};
+pub fn getFields(opcode: Opcode, assembly: []const u8, idx: usize) Fields {
+    const prefix: u8 = assembly[idx];
+    return switch (opcode) {
+        .mov_rm => blk: {
+            const infix: Infix = @bitCast(assembly[idx + 1]);
+            const reg_is_dest: bool = prefix & 0b0000_0010 != 0;
+            break :blk Fields{
+                .dest = if(reg_is_dest) infix.reg else infix.rm,
+                .src = if(reg_is_dest) infix.rm else infix.reg,
+                .wide = prefix & 0b0000_0001 != 0,
+                .rm = infix.rm,
+                .reg = infix.reg,
+                .mod = infix.mod,
+                .len = 2,
+            };
+        },
+        .mov_r_imm => blk: {
+            const wide: bool = prefix & 0b0000_1000 != 0;
+            const data_size: u3 = if(wide) 2 else 1;
+            const reg: u3 = @truncate(prefix & 0b0000_0111);
+            // TODO: implement 16-bit immediate.
+            // std.mem.bytesAsValue(u16, assembly[idx+1..][0..2]).*
+            break :blk Fields{
+                .dest = reg,
+                .wide = wide,
+                .reg = reg,
+                .data = if(wide) 0 else assembly[idx + 1],
+                .len = 1 + data_size,
+            };
+        },
+        else => unreachable,
+    };
+}
 
 pub fn writeMnemonic(alloc: std.mem.Allocator, assembly: []const u8) ![]u8 {
     var array_list = try std.ArrayList(u8).initCapacity(alloc, 100);
     defer array_list.deinit(alloc);
 
-    // TODO: Instructions can be different length.
-    const byte_code: []align(1) const Instruction = std.mem.bytesAsSlice(Instruction, assembly);
     try array_list.print(alloc, "bits 16\n", .{});
-    for (byte_code) |inst| {
-        try array_list.print(alloc, "{f}\n", .{ inst });
+
+    var idx: usize = 0;
+    while(idx < assembly.len) {
+        const prefix: u8 = assembly[idx];
+        const opcode: Opcode = getOpcode(prefix);
+        const fields: Fields = getFields(opcode, assembly, idx);
+
+        const opcode_supported: bool = opcode == .mov_rm or opcode == .mov_r_imm;
+        const fields_supported: bool = fields.mod == null or fields.mod.? == .register;
+        if(!opcode_supported or !fields_supported) {
+            return array_list.toOwnedSlice(alloc);
+        }
+
+        try array_list.print(alloc, "{f} ", .{ opcode });
+
+        const dest_tag = if(fields.wide) @tagName(@as(RegFieldWide, @enumFromInt(fields.dest))) 
+            else @tagName(@as(RegField, @enumFromInt(fields.dest)));
+        try array_list.print(alloc, "{s}, ", .{ dest_tag });
+
+        switch (opcode) {
+            .mov_rm => {
+                const src_tag = if(fields.wide) @tagName(@as(RegFieldWide, @enumFromInt(fields.src.?))) 
+                    else @tagName(@as(RegField, @enumFromInt(fields.src.?)));
+                try array_list.print(alloc, "{s}", .{ src_tag });
+            },
+            .mov_r_imm => {
+                try array_list.print(alloc, "{d}", .{ fields.data.? });
+            },
+            else => unreachable,
+        }
+        try array_list.print(alloc, "\n", .{});
+
+        idx += fields.len;
     }
 
     return array_list.toOwnedSlice(alloc);
