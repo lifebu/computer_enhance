29572c69a9cc20297c9719bd5e2993eee89785c3
Christian Burmeister <mail@cburmeister.me>
2025-11-23 11:59:10 +0100

    Tests: Removed diff and grep dependency.

--- src/main.zig
+++ src/main.zig
@@ -33,16 +33,22 @@ pub fn main() !void {
     }
 }
 
-fn runCommand(argv: []const []const u8, allocator: std.mem.Allocator) ![]u8 {
-    var cmd = std.process.Child.init(argv, allocator);
-    cmd.stdout_behavior = .Pipe;
-    try cmd.spawn();
+fn dumpTextDiff(expected: []const u8, actual: []const u8) void {
+    var expected_lines = std.mem.splitScalar(u8, expected, '\n');
+    var actual_lines = std.mem.splitScalar(u8, actual, '\n');
 
-    const result = try cmd.stdout.?.readToEndAlloc(allocator, std.math.maxInt(u32));
-    errdefer allocator.free(result);
+    while(expected_lines.peek() != null or actual_lines.peek() != null) {
+        const expected_line = if(expected_lines.next()) |nxt| nxt else "";
+        if(std.mem.startsWith(u8, expected_line, ";") or expected_line.len == 0) {
+            continue;
+        }
 
-    _ = try cmd.wait();
-    return result;
+        const actual_line = if(actual_lines.next()) |nxt| nxt else "";
+        const is_equal: bool = std.mem.eql(u8, expected_line, actual_line);
+        const divider = if(is_equal) " " else "|";
+
+        std.debug.print("{s:<30} {s} {s:<30}\n", .{ expected_line, divider, actual_line });
+    }
 }
 
 test "decode" {
@@ -50,8 +56,6 @@ test "decode" {
     const test_folder = "upstream/perfaware/part1/";
     const bin_actual_file = "results/result";
     const asm_actual_file = "results/result.asm";
-    const grep_asm_expected_file = "results/grep_expected.asm";
-    const grep_asm_actual_file = "results/grep_result.asm";
     const test_names = [_][]const u8{
         "listing_0037_single_register_mov",
         "listing_0038_many_register_mov",
@@ -59,41 +63,28 @@ test "decode" {
     };
 
     for (test_names, 0..) |test_name, test_idx| {
-        // create new binary.
         const bin_file = try std.fmt.allocPrint(gpa, "{s}{s}", .{ test_folder, test_name });
         defer gpa.free(bin_file);
         const bin_expected = try std.fs.cwd().readFileAlloc(gpa, bin_file, std.math.maxInt(u32));
         defer gpa.free(bin_expected);
+
+        const asm_file = try std.fmt.allocPrint(gpa, "{s}.asm", .{ bin_file });
+        defer gpa.free(asm_file);
+        const asm_expected = try std.fs.cwd().readFileAlloc(gpa, asm_file, std.math.maxInt(u32));
+        defer gpa.free(asm_expected);
 
         // TODO: Do that for each line (first create instructions struct, then print to buffer).
         const asm_actual: []u8 = try part1.writeMnemonic(gpa, bin_expected);
         defer gpa.free(asm_actual);
         try std.fs.cwd().writeFile(.{ .data = asm_actual, .sub_path = asm_actual_file, .flags = .{} });
 
-        const nasm_result = try runCommand(&[_][]const u8{ "nasm", asm_actual_file }, gpa);
-        defer gpa.free(nasm_result);
+        var nasm = std.process.Child.init(&[_][]const u8{ "nasm", asm_actual_file }, gpa);
+        try nasm.spawn();
+        _ = try nasm.wait();
 
         const bin_actual = try std.fs.cwd().readFileAlloc(gpa, bin_actual_file, std.math.maxInt(u32));
         defer gpa.free(bin_actual);
 
-        // create text diff
-        const asm_file = try std.fmt.allocPrint(gpa, "{s}.asm", .{ bin_file });
-        defer gpa.free(asm_file);
-        const asm_expected = try std.fs.cwd().readFileAlloc(gpa, asm_file, std.math.maxInt(u32));
-        defer gpa.free(asm_expected);
-
-        const grep_asm_expected = try runCommand(&[_][]const u8{ "grep", "-Ev", "^\\s*(;|$)", asm_file }, gpa);
-        defer gpa.free(grep_asm_expected);
-        try std.fs.cwd().writeFile(.{ .data = grep_asm_expected, .sub_path = grep_asm_expected_file, .flags = .{} });
-
-        const grep_asm_actual = try runCommand(&[_][]const u8{ "grep", "-Ev", "^\\s*(;|$)", asm_actual_file }, gpa);
-        defer gpa.free(grep_asm_actual);
-        try std.fs.cwd().writeFile(.{ .data = grep_asm_actual, .sub_path = grep_asm_actual_file, .flags = .{} });
-
-        const diff_result = try runCommand(&[_][]const u8{ "diff", "-y", grep_asm_actual_file, grep_asm_expected_file }, gpa);
-        defer gpa.free(diff_result);
-
-        // tests
         errdefer {
             std.debug.print("Test: {d}: {s}\n", .{ test_idx, bin_file });
             std.debug.print("expected:\n", .{});
@@ -101,7 +92,8 @@ test "decode" {
             std.debug.print("actual:\n", .{});
             std.debug.dumpHex(bin_actual);
             std.debug.print("diff:\n", .{});
-            std.debug.print("{s}\n", .{ diff_result });
+            std.debug.print("manual diff:\n", .{});
+            dumpTextDiff(asm_expected, asm_actual);
         }
         const min_len = @min(bin_expected.len, bin_actual.len);
         for (bin_expected[0..min_len], bin_actual[0..min_len]) |expected, actual| {
