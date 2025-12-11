64db3c34a526600c9390d204549e8af1a782fae0
lifebu <mail@cburmeister.me>
2025-12-11 20:42:55 +0100

    - Started with writing the simple json parser.

--- src/haversine/Json.zig
+++ src/haversine/Json.zig
@@ -10,16 +10,11 @@ value: ?[]const u8 = undefined,
 first_child: ?*Self = null,
 next_sibling: ?*Self = null,
 
-// TODO:
-// {} => Struct
-    // "label" : ... 
-// [] => Array
-
 pub const Error = error {
     UnknownToken,
 };
 
-pub fn init(alloc: std.mem.Allocator, buff: []const u8) Error!Self {
+pub fn init(alloc: std.mem.Allocator, buff: []const u8) !Self {
     return try parse(alloc, buff);
 }
 
@@ -75,21 +70,73 @@ const Token = enum {
     comma,
 };
 
+const ParentStack = struct {
+    curr: *Self,
+    prev: *Self,
+};
+
 fn parse(alloc: std.mem.Allocator, buff: []const u8) !Self {
+    var root: Self = .{ .is_root = true };
+    var curr_parent: *Self = &root;
+    var curr_child: ?*Self = null;
+
     var idx: usize = 0;
     while(idx < buff.len) {
         const char: u8 = buff[idx];
         switch(char) {
-            //"{" => ,
-            //else => return Error.UnknownToken,
-            else => return .{ .is_root = true },
+            '{', '[' => { // open a new child
+                // TODO: When to switch curr_parent?
+                // Probably need a "stack" of parents.
+                const new_child: *Self = try alloc.create(Self);
+                if (curr_parent.first_child == null) {
+                    curr_parent.first_child = new_child;
+                    curr_child = new_child;
+                } else {
+                    curr_child.?.next_sibling = new_child;
+                    curr_child = new_child;
+                }
+            },
+            '"' => { // start label
+                assert(curr_child != null);
+                var next_idx: usize = idx + 1;
+                while(next_idx < buff.len) : (next_idx += 1) {
+                    if(buff[next_idx] == '"') {
+                        break;
+                    }
+                }
+
+                const label: []const u8 = buff[(idx + 1)..next_idx];
+                curr_child.?.label = label;
+                idx = next_idx;
+            },
+            ':' => {
+                var next_idx: usize = idx + 1;
+                var found_comma: bool = false;
+                while(next_idx < buff.len) : (next_idx += 1) {
+                    if (buff[next_idx] == '[') {
+                        break;
+                    } else if (buff[next_idx] == ',') {
+                        found_comma = true;
+                        break;
+                    }
+                }
+
+                // TODO: We don't have , all the time (last element).
+                if(found_comma) {
+                    assert(curr_child != null);
+                    // TODO: Remove whitespace from this?
+                    curr_child.?.value = buff[(idx + 1)..next_idx];
+                }
+            },
+            '}', ']' => { // close current child => go back to parent?
+
+            },
+            '\n', '\t', ' ', ',' => {},
+            else => return Error.UnknownToken,
         }
 
         idx += 1;
     }
-    _ = alloc;
-    return .{
 
-        .is_root = true,
-    };
+    return root;
 }
